d835967425c527aa85e68afc5714a59653693670
AhmadTechB <125239830+AhmadTechB@users.noreply.github.com>
2023-05-11 20:35:12 +0530

    todaywork

--- lib/screen/login_home.dart
+++ lib/screen/login_home.dart
@@ -1,10 +1,66 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-class MyHomePageLogin extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:http/http.dart'  as http;
+
+class MyHomePageLogin extends StatefulWidget {
   const MyHomePageLogin({Key? key, required String title}) : super(key: key);
 
   @override
+  State<MyHomePageLogin> createState() => _MyHomePageLoginState();
+}
+
+class _MyHomePageLoginState extends State<MyHomePageLogin> {
+  int a = 0;
+
+  List<dynamic>? getData;
+  
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Rest API Call $a"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+        fetchUsers();
+        decreament();
+        },
+      ),
+      body: GestureDetector(
+        child: Center(child: Text('increament')),
+        onTap: (){
+          text();
+        },
+      ),
+    );
+  }
+
+  void fetchUsers() async{
+
+    final response = await http.get(Uri.parse('https://randomuser.me/api/?results=10'));
+
+    if(response.statusCode == 200 ){
+
+    var jsonD =  jsonDecode(response.body);
+    
+    getData = jsonD['results'];
+
+    }
+    else{
+
+      print('error');
+    }
+  }
+
+  void text(){
+    setState(() {
+      a++;
+    });
+  }
+  void decreament(){
+    setState(() {
+      a--;
+    });
   }
 }
