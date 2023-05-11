c4268dc489e9df5bd137bd2c2c6a157eeb51810a
AhmadTechB <125239830+AhmadTechB@users.noreply.github.com>
2023-05-11 12:26:03 +0530

    initial

---
+++ lib/screen/login_home.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MyHomePageLogin extends StatelessWidget {
+  const MyHomePageLogin({Key? key, required String title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
